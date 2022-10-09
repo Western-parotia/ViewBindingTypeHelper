b6330fdb45d561c7ec57dc6694345eb1f9307aa0
WangNeng <873456775@qq.com>
2022-10-09 19:46:10 +0800

    版本1.0.3，增加根据泛型index直接获取泛型类型

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 首先要配置腾讯云的maven，一般都有，不多说
 ```
-"com.foundation.widget:view-binding-helper:1.0.2"
+"com.foundation.widget:view-binding-helper:1.0.3"
 ```
--- widget/src/main/java/com/foundation/widget/binding/ViewBindingHelper.kt
+++ widget/src/main/java/com/foundation/widget/binding/ViewBindingHelper.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.viewbinding.ViewBinding
 import java.lang.reflect.ParameterizedType
 
+@Suppress("UNCHECKED_CAST")
 object ViewBindingHelper {
     /**
      * 缓存已找到的class
@@ -22,14 +23,12 @@ object ViewBindingHelper {
             return null
         }
         cacheClass[myClass.name]?.let {
-            @Suppress("UNCHECKED_CAST")
             return it as Class<T>
         }
         //遍历父类所有泛型参数
         (myClass.genericSuperclass as? ParameterizedType)?.actualTypeArguments?.forEach { type ->
             if (type is Class<*>) {
                 if (ViewBinding::class.java.isAssignableFrom(type)) {
-                    @Suppress("UNCHECKED_CAST")
                     val clazz = type as Class<T>
                     cacheClass[myClass.name] = clazz//缓存
                     return clazz
@@ -38,6 +37,37 @@ object ViewBindingHelper {
         }
         //继续循环父类查询
         return getViewBindingClass(myClass.superclass)
+    }
+
+    /**
+     * 获取ViewBinding的class，直接指定泛型位置
+     *
+     * @param myClass 当前类的class（getClass()）
+     * @param typeIndex 泛型的位置，如：你的泛型在第二个位置传1
+     */
+    fun <T : ViewBinding> getViewBindingClassFromIndex(
+        myClass: Class<*>?,
+        typeIndex: Int
+    ): Class<T>? {
+        if (myClass == Any::class.java || myClass == null) {
+            return null
+        }
+        cacheClass[myClass.name]?.let {
+            return it as Class<T>
+        }
+        //直接取对应index的泛型参数
+        ((myClass.genericSuperclass as? ParameterizedType)
+            ?.actualTypeArguments
+            ?.getOrNull(typeIndex) as? Class<*>)
+            ?.let {
+                if (ViewBinding::class.java.isAssignableFrom(it)) {
+                    val clazz = it as Class<T>
+                    cacheClass[myClass.name] = clazz//缓存
+                    return clazz
+                }
+            }
+        //继续循环父类查询
+        return getViewBindingClassFromIndex(myClass.superclass, typeIndex)
     }
 
     /**
@@ -69,7 +99,6 @@ object ViewBindingHelper {
                 "inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java
             )
 
-            @Suppress("UNCHECKED_CAST")
             val vb = method.invoke(null, layoutInflater, container, attachToParent) as T
             //保存自身，方便其他框架使用
             vb.root.setTag(R.id.tag_view_binding_obj, vb)
